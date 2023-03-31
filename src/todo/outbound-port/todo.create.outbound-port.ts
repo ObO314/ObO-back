90de60a62471bfde42780614ec6fe94ccbf166d4
LYUHIT <uiseok0514@gmail.com>
2023-03-31 22:27:21 +0900

    CREATE : TODO

---
+++ src/todo/outbound-port/todo.create.outbound-port.ts
@@ -0,0 +1,24 @@
+import { Todos } from 'src/database/entities/Todos';
+import { Users } from 'src/database/entities/Users';
+
+export type TodoCreateOutboundPortInputDto = {
+  userId: Users;
+
+  name: string;
+
+  startTime: Date;
+
+  endTime: Date;
+
+  completed: boolean;
+};
+
+export type TodoCreateOutboundPortOutputDto = Todos;
+
+export const TODO_CREATE_OUTBOUND_PORT = 'TODO_CREATE_OUTBOUND_PORT' as const;
+
+export interface TodoCreateOutboundPort {
+  create(
+    params: TodoCreateOutboundPortInputDto,
+  ): Promise<TodoCreateOutboundPortOutputDto>;
+}
