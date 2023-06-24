78c521a3be1e410071c4bc60e2d6f86491a3e3eb
LYUHIT <uiseok0514@gmail.com>
2023-06-24 23:31:23 +0900

    UPDATE : SocialLogin merge with SocialSignUp

--- src/auth/outbound-port/auth.google.strategy.outbound-port.ts
+++ src/auth/outbound-port/auth.google.strategy.outbound-port.ts
@@ -4,6 +4,13 @@ export type AuthGoogleStrategyOutboundPortInputDto = {};
 
 export type AuthGoogleStrategyOutboundPortOutputDto = Users;
 
+export type AuthGoogleStrategyOutboundSignUpPortInputDto = {
+  email: string;
+  nickname: string;
+  authMethod: string;
+};
+export type AuthGoogleStrategyOutboundSignUpPortOutputDto = Users;
+
 export const AUTH_GOOGLE_STRATEGY_OUTBOUND_PORT =
   'AUTH_GOOGLE_STRATEGY_OUTBOUND_PORT' as const;
 
@@ -11,4 +18,8 @@ export interface AuthGoogleStrategyOutboundPort {
   findUser(
     email: AuthGoogleStrategyOutboundPortInputDto,
   ): Promise<AuthGoogleStrategyOutboundPortOutputDto>;
+
+  signUp(
+    params: AuthGoogleStrategyOutboundSignUpPortInputDto,
+  ): Promise<AuthGoogleStrategyOutboundSignUpPortOutputDto>;
 }
