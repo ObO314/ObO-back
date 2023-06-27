76fbe14cdda8879f80a7b7ae24ff47fb496b9c6e
LYUHIT <uiseok0514@gmail.com>
2023-06-27 14:38:42 +0900

    UPDATE : SocialLogin get GoogleCode

--- src/modules/auth/guard/auth.google.guard.ts
+++ src/modules/auth/guard/auth.google.guard.ts
@@ -5,17 +5,15 @@ import { parse as parseQuery } from 'querystring';
 
 @Injectable()
 export class AuthGoogleGuard extends AuthGuard('google') {
-  canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-
-    // 백엔드에서 구글 승인코드 바로 받을 때 활성화
-    // const { url } = request;
-    // const { query } = parse(url, true);
-    // const code = query.code;
-
-    const { code } = request.body;
-    request.body = { ...request.body, code: code };
-
-    return super.canActivate(context);
-  }
+  // canActivate(context: ExecutionContext) {
+  //   const request = context.switchToHttp().getRequest();
+  //   // 백엔드에서 구글 승인코드 바로 받을 때 활성화
+  //   const { url } = request;
+  //   const { query } = parse(url, true);
+  //   const code = query.code;
+  //   // const { code } = request.body;
+  //   request.body = { ...request.body, code: code };
+  //   console.log(code);
+  //   return super.canActivate(context);
+  // }
 }
--- src/modules/routine/outbound-adaptor/routine.read-by-date.repository.ts
+++ src/modules/routine/outbound-adaptor/routine.read-by-date.repository.ts
@@ -28,59 +28,14 @@ export class RoutineReadByDateRepository
     params: RoutineReadByDateOutboundPortInputDto,
   ): Promise<RoutineReadByDateOutboundPortOutputDto> {
     const em = this.em;
-    const userRoutines = await em.find(Routines, { user: params.userId });
-
     const qb = await em
       .createQueryBuilder(RoutineHistories, 'rh')
-      .select('*')
-      .where({ user: params.userId })
-      .andWhere({ updatedTime: { $lte: params.date } })
-      .andWhere({
-        $or: [{ inactiveDate: null }, { inactiveDate: { $lte: params.date } }],
-      })
-      // .groupBy('rh.routine')
-      .orderBy({ updatedTime: 'DESC' });
-    // .limit(1);
-    // .execute();
-
-    // `
-    //   SELECT *
-    //   FROM routineHistories
-    //   WHERE user = ${params.userId}
-    //     AND ( updatedTime < ${params.date} )
-    //     AND ( updatedTime IS NULL OR ${params.date} < inactiveDate )
-    //   GROUP BY routine
-    //   ORDER BY updatedTime
-    //   LIMIT 1
-    //   `;
-
+      .select('rh.routine')
+      .addSelect('MAX(rh.updated_at) as updated_at')
+      .where({ user: params.userId, updatedAt: { $lte: params.date } })
+      .groupBy('rh.routine')
+      .execute();
     console.log(qb);
-
-    // return await pipe(
-    //   userRoutines,
-    //   toAsync,
-    //   map((userRoutine) =>
-    //     em.find(
-    //       RoutineHistories,
-    //       { routine: userRoutine },
-    //       { orderBy: { updatedTime: QueryOrder.DESC }, limit: 1 },
-    //     ),
-    //   ),
-    //   filter(
-    //     (routineHistory) =>
-    //       routineHistory[0].inactiveDate > params.date ||
-    //       routineHistory[0].inactiveDate == null,
-    //   ),
-    //   map((filteredRoutines) =>
-    //     em.findOne(
-    //       Routines,
-    //       { id: filteredRoutines[0].routine.id },
-    //       { populate: ['routineRecords', 'routineHistories'] },
-    //     ),
-    //   ),
-    //   concurrent(10),
-    //   toArray,
-    // );
     return;
   }
 }
